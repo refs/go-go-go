cd58b0d4087faf7788d554caa11e95d6140ef4d0
A.Unger <zyxancf@gmail.com>
2020-02-18 13:51:35 +0100

    cleanup root cmd

--- main.go
+++ main.go
@@ -2,15 +2,6 @@ package main
 
 import "github.com/refs/go-go-go/cmd"
 
-// import "github.com/google/go-github/v29/github"
-
 func main() {
-	// configure git
-	// parse repos.csv
-	// iterate repos
-	// run template
-	// generate README.md
-	// push README.md
-	// client := github.NewClient(nil)
 	cmd.RootCommand()
 }
